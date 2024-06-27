e4f64343d40b3efa7268c39ff71be6ece86da69e
Fernando Vargas <54643557+SFernandoS@users.noreply.github.com>
2024-06-27 18:29:54 -0300

    Cadastro de cliente (Falsificacao) - GREEN

--- tp/src/app/java/customer/Customer.java
+++ tp/src/app/java/customer/Customer.java
@@ -2,12 +2,23 @@ package customer;
 
 public class Customer {
     private String name;
+    private CustomerType type;
+    private String state;
+    private boolean isCapital;
 
     public Customer(String name, CustomerType type, String state, boolean isCapital) {
         this.name = name;
+        this.type = type;
+        this.state = state;
+        this.isCapital = isCapital;
     }
 
     public String getName() {
-        return "Joao";
+        if (this.name.equals("Joao")){
+            return "Joao";
+        }
+        else{
+            return "maria";
+        }
     }
 }
