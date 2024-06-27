62d9d38adb03921afc6a7b651abd7e15d5c118e0
Fernando Vargas <54643557+SFernandoS@users.noreply.github.com>
2024-06-27 18:18:02 -0300

    Implementacao da funcionalidade usando a falsificaçao - GREEN TDD

---
+++ tp/src/app/java/customer/Customer.java
@@ -0,0 +1,13 @@
+package customer;
+
+public class Customer {
+    private String name;
+
+    public Customer(String name, CustomerType type, String state, boolean isCapital) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return "Joao";
+    }
+}
---
+++ tp/src/app/java/customer/CustomerType.java
@@ -0,0 +1,7 @@
+package customer;
+
+public enum CustomerType {
+    STANDARD,
+    SPECIAL,
+    PRIME
+}
