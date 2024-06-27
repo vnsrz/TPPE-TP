d1b6370c07a15bfc8227bdf84a239ae66dc0edfb
Fernando Vargas <54643557+SFernandoS@users.noreply.github.com>
2024-06-27 13:15:50 -0300

    Estrutura básica do projeto

---
+++ tp/src/app/java/main/App.java
@@ -0,0 +1,11 @@
+package app.java.main;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(getHelloWorld());
+    }
+
+    public static String getHelloWorld() {
+        return "Hello, World!";
+    }
+}
