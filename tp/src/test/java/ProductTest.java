e3962a25e28e0f8ed814fd5e90acd00929dc8e9d
vnsrz <viniciusroriz21@gmail.com>
2024-06-28 21:43:24 -0300

    Parametrização dos testes do produto

--- tp/src/test/java/ProductTest.java
+++ tp/src/test/java/ProductTest.java
@@ -1,29 +1,46 @@
-import product.Product;
+package product;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class ProductTest {
-    @Test
-    public void testCreateProductCode() {
-        Product product = new Product(1, "Bola de praia", 15.5, "unidade");
-        assertEquals(1, product.getCode());
+    private int testCode;
+    private String testDescription;
+    private double testPrice;
+    private String testUnit;
+
+    public ProductTest(int code, String description, double price, String unit) {
+        this.testCode = code;
+        this.testDescription = description;
+        this.testPrice = price;
+        this.testUnit = unit;
+    }
+
+    @Parameters(name = "{index}: Product({0}, {1}, {2}, {3})")
+    public static Collection<Object[]> parameters() {
+        return Arrays.asList(new Object[][] {
+            {1, "Bola de praia", 15.5, "unidade"},
+            {2, "Bastão de cola", 5, "unidade"},
+            {3, "Tecido azul", 9.99, "metro"},
+            {4, "Corda trançada", 5, "metro"}
+        });
     }
 
     @Test
-    public void testCreateProductDescription() {
-        Product product = new Product(2, "Bastão de cola", 5, "unidade");
-        assertEquals("Bastão de cola", product.getDescription());
-    }
+    public void testProduct() {
+        Product product = new Product(testCode, testDescription, testPrice, testUnit);
 
-    @Test
-    public void testCreateProductPrice() {
-        Product product = new Product(3, "Tecido azul", 9.99, "metro");
-        assertEquals(9.99, product.getPrice(), 0);
-    }
-
-    @Test
-    public void testCreateProductUnit() {
-        Product product = new Product(4, "Corda trançada", 5, "metro");
-        assertEquals("metro", product.getUnit());
+        assertEquals(testCode, product.getCode());
+        assertEquals(testDescription, product.getDescription());
+        assertEquals(testPrice, product.getPrice(), 0);
+        assertEquals(testUnit, product.getUnit());
     }
 }
