87e34851652ab89259c3ed85a1a2b5e5a0b9a1fe
vnsrz <viniciusroriz21@gmail.com>
2024-06-28 21:04:46 -0300

    GREEN - implementação por falsificação

--- tp/src/app/java/product/Product.java
+++ tp/src/app/java/product/Product.java
@@ -2,12 +2,18 @@ package product;
 
 public class Product {
     public int code;
+    public String description;
 
     public Product(int code, String description, double price, String unit) {
         this.code = code;
+        this.description = description;
     }
 
     public int getCode() {
         return this.code;
+    }
+
+    public String getDescription() {
+        return "Bastão de cola";
     }
 }
