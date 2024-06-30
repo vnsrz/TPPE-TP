5cf26a85c5ff7deb0792378bbd9d73afc3ea876c
Vitormanoel17 <vitormanoel.aquino@gmail.com>
2024-06-30 00:22:12 -0300

    RED: teste data de venda

---
+++ tp/src/test/java/SaleTest.java
@@ -0,0 +1,16 @@
+import Sale.Sale;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.Instant;
+import java.util.Date;
+
+public class SaleTest {
+
+    @Test
+    public void testGetSaleDate() {
+        Sale sale = new Sale(Date.from(Instant.parse("2019-12-03T10:15:30.00Z")));
+        Assert.assertEquals(sale.getDate(), Date.from(Instant.parse("2019-12-03T10:15:30.00Z")));
+    }
+
+}
