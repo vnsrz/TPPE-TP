8f080fc72ae5534eca781fb045d13038cc702d72
Vitormanoel17 <vitormanoel.aquino@gmail.com>
2024-06-30 01:29:23 -0300

    repete processo TDD para outros atributos da classe Sale

--- tp/src/app/java/Sale/Sale.java
+++ tp/src/app/java/Sale/Sale.java
@@ -1,16 +1,38 @@
 package Sale;
 
-import java.time.Instant;
+import customer.Customer;
+import product.Product;
+
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Sale {
     private Date date;
+    private Customer customer;
+    private ArrayList<Product> product;
+    private String paymentMethod;
 
-    public Sale(Date date) {
+    public Sale(Date date, Customer customer, ArrayList<Product> product, String paymentMethod) {
         this.date = date;
+        this.customer = customer;
+        this.product = product;
+        this.paymentMethod = paymentMethod;
     }
 
     public Date getDate() {
         return this.date;
     }
+
+    public Customer getCustomer() {
+        return this.customer;
+    }
+
+    public ArrayList<Product> getProduct() {
+        return this.product;
+    }
+
+    public String getPaymentMethod() {
+        return this.paymentMethod;
+    }
+
 }
--- tp/src/test/java/SaleTest.java
+++ tp/src/test/java/SaleTest.java
@@ -1,22 +1,100 @@
 import Sale.Sale;
+import customer.Customer;
+import customer.CustomerType;
 import org.junit.Assert;
 import org.junit.Test;
+import product.Product;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class SaleTest {
 
     @Test
     public void testGetSaleDate() {
-        Sale sale = new Sale(Date.from(Instant.parse("2019-12-03T10:15:30.00Z")));
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Bicicleta",300,"unidade"));
+        products.add(new Product(2,"Camisa",50,"unidade"));
+        Customer costumer = new Customer("Julia", CustomerType.STANDARD, "GO",false);
+        Sale sale = new Sale(Date.from(Instant.parse("2019-12-03T10:15:30.00Z")),costumer, products, "1234567890123456");
         Assert.assertEquals(sale.getDate(), Date.from(Instant.parse("2019-12-03T10:15:30.00Z")));
     }
 
     @Test
     public void testGetDifferentSaleDate() {
-        Sale sale = new Sale(Date.from(Instant.parse("2024-12-14T10:15:30.00Z")));
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Bicicleta",300,"unidade"));
+        products.add(new Product(2,"Camisa",50,"unidade"));
+        Customer costumer = new Customer("Julia", CustomerType.STANDARD, "GO",false);
+        Sale sale = new Sale(Date.from(Instant.parse("2024-12-14T10:15:30.00Z")), costumer, products,"1234567890123456");
         Assert.assertEquals(sale.getDate(), Date.from(Instant.parse("2024-12-14T10:15:30.00Z")));
     }
+    @Test
+    public void testGetCustomer() {
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Bicicleta",300,"unidade"));
+        products.add(new Product(2,"Camisa",50,"unidade"));
+        Customer costumer = new Customer("Julia", CustomerType.STANDARD, "GO",false);
+        Sale sale = new Sale(Date.from(Instant.now()), costumer, products, "1234567890123456");
+        Assert.assertEquals(sale.getCustomer(), costumer);
+
+    }
+
+    @Test
+    public void testGetCustomerPrime() {
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Bicicleta",300,"unidade"));
+        products.add(new Product(2,"Camisa",50,"unidade"));
+        Customer costumer = new Customer("Julia", CustomerType.PRIME, "GO",false);
+        Sale sale = new Sale(Date.from(Instant.now()), costumer, products, "1234567890123456");
+        Assert.assertEquals(sale.getCustomer(), costumer);
+    }
+
+    @Test
+    public void testGetProducts() {
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Bicicleta",300,"unidade"));
+        products.add(new Product(2,"Camisa",50,"unidade"));
+
+        Customer costumer = new Customer("Julia", CustomerType.PRIME, "GO",false);
+        Sale sale = new Sale(Date.from(Instant.now()), costumer,products, "1234567890123456");
+        Assert.assertEquals(sale.getProduct(), products);
+    }
+
+    @Test
+    public void testGetProduct() {
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Bicicleta",300,"unidade"));
+
+        Customer costumer = new Customer("Julia", CustomerType.PRIME, "GO",false);
+        Sale sale = new Sale(Date.from(Instant.now()), costumer,products, "1234567890123456");
+        Assert.assertEquals(sale.getProduct(), products);
+    }
+
+    @Test
+    public void testGetPaymentMethod() {
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Bicicleta",300,"unidade"));
+        products.add(new Product(2,"Camisa",50,"unidade"));
+
+        Customer costumer = new Customer("Julia", CustomerType.PRIME, "GO",false);
+        Sale sale = new Sale(Date.from(Instant.now()), costumer,products, "1234567890123456");
+        Assert.assertEquals(sale.getPaymentMethod(), "1234567890123456");
+    }
+
+    @Test
+    public void testGetDifferentPaymentMethod() {
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Bicicleta",300,"unidade"));
+        products.add(new Product(2,"Camisa",50,"unidade"));
+
+        Customer costumer = new Customer("Julia", CustomerType.PRIME, "GO",false);
+        Sale sale = new Sale(Date.from(Instant.now()), costumer,products, "1234567890123478");
+        Assert.assertEquals(sale.getPaymentMethod(), "1234567890123478");
+    }
+
+
+
 
 }
