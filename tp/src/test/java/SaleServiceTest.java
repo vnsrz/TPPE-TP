931cb7b29c94e9abcb940ee85c68fb9162465916
Vitormanoel17 <vitormanoel.aquino@gmail.com>
2024-06-30 17:02:51 -0300

    RED: duplicação cadastro de venda

--- tp/src/test/java/SaleServiceTest.java
+++ tp/src/test/java/SaleServiceTest.java
@@ -41,4 +41,27 @@ public class SaleServiceTest {
         Assert.assertEquals(saleService.getSales(), sale);
     }
 
+    @Test
+    public void testAddSales() {
+        ArrayList<Product> productsA = new ArrayList<>();
+        productsA.add(new Product(1, "Bicicleta",300,"unidade"));
+        Customer customerA = new Customer("Juliana", CustomerType.STANDARD, "Valparaiso",false);
+        Sale saleA = new Sale(Date.from(Instant.now()), customerA, productsA, "0234567890123456");
+
+        ArrayList<Product> productsB = new ArrayList<>();
+        productsB.add(new Product(2,"Camisa",50,"unidade"));
+        Customer customerB = new Customer("Adriano", CustomerType.STANDARD, "Gama",false);
+        Sale saleB = new Sale(Date.from(Instant.now()), customerB, productsB, "0234567890123456");
+
+        saleService.addSale(saleA);
+        saleService.addSale(saleB);
+
+        Assert.assertEquals(saleService.getSales(),saleA);
+        Assert.assertEquals(saleService.getSales(), saleB);
+
+    }
+
+
+
+
 }
