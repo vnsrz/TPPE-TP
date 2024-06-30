2fb21ab852b78c211e737f412b34188184891724
Vitormanoel17 <vitormanoel.aquino@gmail.com>
2024-06-30 17:37:00 -0300

    Parametrizando testes de cadastro de venda

--- tp/src/test/java/SaleServiceTest.java
+++ tp/src/test/java/SaleServiceTest.java
@@ -5,6 +5,9 @@ import customer.CustomerType;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import product.Product;
 
 import java.util.Date;
@@ -18,9 +21,19 @@ import java.util.ArrayList;
 // calcular impostos
 // calcular cashback
 
+@RunWith(Parameterized.class)
 public class SaleServiceTest {
 
     SaleService saleService;
+    Object[] sales;
+
+    Object sale;
+
+
+    public SaleServiceTest(Object[] sales, Object sale) {
+        this.sales = sales;
+        this.sale = sale;
+    }
 
     @Before
     public void setup() {
@@ -28,41 +41,8 @@ public class SaleServiceTest {
     }
 
 
-    @Test
-    public void testAddSale() {
-        ArrayList<Product> products = new ArrayList<>();
-        products.add(new Product(1, "Bicicleta",300,"unidade"));
-        products.add(new Product(2,"Camisa",50,"unidade"));
-        Customer customer = new Customer("Juliana", CustomerType.STANDARD, "Valparaiso",false);
-        Sale sale = new Sale(Date.from(Instant.now()), customer, products, "0234567890123456");
-
-        saleService.addSale(sale);
-
-        Assert.assertTrue(saleService.getSales().contains(sale));
-    }
-
-    @Test
-    public void testAddSales() {
-        ArrayList<Product> productsA = new ArrayList<>();
-        productsA.add(new Product(1, "Bicicleta",300,"unidade"));
-        Customer customerA = new Customer("Juliana", CustomerType.STANDARD, "Valparaiso",false);
-        Sale saleA = new Sale(Date.from(Instant.now()), customerA, productsA, "0234567890123456");
-
-        ArrayList<Product> productsB = new ArrayList<>();
-        productsB.add(new Product(2,"Camisa",50,"unidade"));
-        Customer customerB = new Customer("Adriano", CustomerType.STANDARD, "Gama",false);
-        Sale saleB = new Sale(Date.from(Instant.now()), customerB, productsB, "0234567890123456");
-
-        saleService.addSale(saleA);
-        saleService.addSale(saleB);
-
-        Assert.assertTrue(saleService.getSales().contains(saleA));
-        Assert.assertTrue(saleService.getSales().contains(saleB));
-
-    }
-
-    @Test
-    public void testAddMoreSales() {
+    @Parameters
+    public static Object[][] getParameters() {
         ArrayList<Product> productsA = new ArrayList<>();
         productsA.add(new Product(1, "Bicicleta",300,"unidade"));
         Customer customerA = new Customer("Juliana", CustomerType.STANDARD, "Valparaiso",false);
@@ -77,14 +57,32 @@ public class SaleServiceTest {
         productsB.add(new Product(2,"Camisa",50,"unidade"));
         Customer customerC = new Customer("Adriano", CustomerType.STANDARD, "Gama",false);
         Sale saleC = new Sale(Date.from(Instant.now()), customerC, productsC, "0234567890123456");
+        return new Object[][] {
+                {
+                    new Object[] {saleA},
+                        saleA
+                },
+                {
+                    new Object[] {saleA,saleB},
+                        saleB
+                },
+                {
+                    new Object[] {saleA, saleB, saleC},
+                        saleC
+                }
+        };
 
-        saleService.addSale(saleA);
-        saleService.addSale(saleB);
-        saleService.addSale(saleC);
-        Assert.assertTrue(saleService.getSales().contains(saleA));
-        Assert.assertTrue(saleService.getSales().contains(saleB));
-        Assert.assertTrue(saleService.getSales().contains(saleC));
     }
+
+    @Test
+    public void testAddSale() {
+        for(Object sale : sales) {
+            saleService.addSale((Sale) sale);
+        }
+
+        Assert.assertTrue(saleService.getSales().contains(sale));
+    }
+
 
 
 
