b90291a112f2795a9987d5040fb7bc04fb485f29
Vitormanoel17 <vitormanoel.aquino@gmail.com>
2024-06-30 22:32:22 -0300

    RED: duplica teste calcula desconto

--- tp/src/test/java/CalculateDiscountTest.java
+++ tp/src/test/java/CalculateDiscountTest.java
@@ -16,5 +16,10 @@ public class CalculateDiscountTest {
         Assert.assertEquals(saleService.calculateDiscount(CustomerType.SPECIAL,3000f), 300f, 0.01);
     }
 
+    @Test
+    public void testCalculateDiscountNoSpecial() {
+        Assert.assertEquals(saleService.calculateDiscount(CustomerType.STANDARD,3000f), 0, 0.01);
+    }
+
 
 }
