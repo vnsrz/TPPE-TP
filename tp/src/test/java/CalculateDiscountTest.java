2972ec15aa20cbe7dc5b86f805af34e2287a7812
Vitormanoel17 <vitormanoel.aquino@gmail.com>
2024-06-30 23:02:58 -0300

    Parametriza teste calcula desconto e cria suite de testes

--- tp/src/test/java/CalculateDiscountTest.java
+++ tp/src/test/java/CalculateDiscountTest.java
@@ -3,37 +3,44 @@ import customer.CustomerType;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class CalculateDiscountTest {
 
     SaleService saleService;
+    CustomerType customerTypeTest;
+    float amountTest;
+    String paymentCardTest;
+    float discountResult;
+
+
+    public CalculateDiscountTest(CustomerType customerTypeTest, float amountTest, String paymentCardTest, float discountResult) {
+        this.customerTypeTest = customerTypeTest;
+        this.amountTest = amountTest;
+        this.paymentCardTest = paymentCardTest;
+        this.discountResult = discountResult;
+    }
     @Before
     public void setup() {
         saleService = new SaleService();
     }
+
+    @Parameters
+    public static Object[][] getParameters() {
+        return new Object[][] {
+                {CustomerType.SPECIAL,3000f, "429611XXXXXXXXXX", 300f},
+                {CustomerType.STANDARD,3000f, "429612XXXXXXXXXX", 0},
+                {CustomerType.PRIME,3000f, "529613XXXXXXXXXX", 0},
+                {CustomerType.PRIME,3000f, "429613XXXXXXXXXX", 300f},
+                {CustomerType.SPECIAL,3000f, "429613XXXXXXXXXX", 600f},
+        };
+    }
     @Test
     public void testCalculateDiscount() {
-        Assert.assertEquals(saleService.calculateDiscount(CustomerType.SPECIAL,3000f, "429611XXXXXXXXXX"), 300f, 0.01);
-    }
-
-    @Test
-    public void testCalculateDiscountNoSpecial() {
-        Assert.assertEquals(saleService.calculateDiscount(CustomerType.STANDARD,3000f, "429612XXXXXXXXXX"), 0, 0.01);
-    }
-
-    @Test
-    public void testCalculateDiscountNoSpeciala() {
-        Assert.assertEquals(saleService.calculateDiscount(CustomerType.PRIME, 3000f, "529613XXXXXXXXXX"), 0, 0.01);
-    }
-
-    @Test
-    public void testCalculateDiscountWithCreditCard() {
-        Assert.assertEquals(saleService.calculateDiscount(CustomerType.PRIME, 3000f, "429613XXXXXXXXXX"), 300f, 0.01);
-    }
-
-    @Test
-    public void testCalculateDiscountWithCreditCardAndSpecial() {
-        Assert.assertEquals(saleService.calculateDiscount(CustomerType.SPECIAL, 3000f, "429613XXXXXXXXXX"), 600f, 0.01);
+        Assert.assertEquals(saleService.calculateDiscount(customerTypeTest,amountTest, paymentCardTest), discountResult, 0.01);
     }
 
 
