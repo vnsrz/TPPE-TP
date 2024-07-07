1be203db48a62a5e69072fafa8988546c1e5623e
daniso0412 <dannysoaresdeoliveira@gmail.com>
2024-07-06 22:41:43 -0300

    correção do test CustomerTest

--- tp/src/test/java/customer/CustomerTest.java
+++ tp/src/test/java/customer/CustomerTest.java
@@ -6,6 +6,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+
+
+
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
